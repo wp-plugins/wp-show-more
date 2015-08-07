81d330bbc0a09af11fc30d449adcd64c0d1d4f89
rene.maurer <rene.maurer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-07 08:46:53 +0000

    Publish new Release 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-show-more/trunk@1214695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-show-more.php
+++ wp-show-more.php
@@ -3,26 +3,31 @@
 Plugin Name: WP show more
 Plugin URI:  http://plugins.wordpress.org/wp-show-more/
 Description: Add a user-defined link to display more content.
-Version:     1.0
+Version:     1.0.1
 Author:      JAMOS Web Service
 Author URI:  http://www.jamos.ch/plugins/wp-show-more
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: WP show more
 */
+
 /*
- * Usage: [show_more title="Read more"] Your hidden Content [/show_more]
+ * Usage: [show_more title="Read more" color="#0066CC"] Your hidden Content [/show_more]
  */
-add_shortcode( 'show_more', 'sm' );
+
+add_shortcode( 'show_more', 'sm');
+
 function sm( $attr, $content ) {
+  if (!isset($attr['color'])) $attr['color'] = '#cc0000';
   $string  = '<div class="show_more">';
-  $string .=' <p class="show_more_title">';
+  $string .=' <p class="show_more_title" style="color: ' . $attr['color'] . ' ">'; 
   $string .= $attr['title'];
   $string .= '</p><div class="show_more_body" style="display:none">';
   $string .= $content;
   $string .= '</div></div>';
   return $string;
 }
+
 add_action( 'wp_enqueue_scripts', 'sm_scripts');
 function sm_scripts (){
   $plugin_url = plugins_url( '/', __FILE__ );
