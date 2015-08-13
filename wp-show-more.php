b1c9c80e79308fd32c538df96b0f753c1fa71740
rene.maurer <rene.maurer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 09:18:03 +0000

    remove instable 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-show-more/trunk@1219773 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-show-more.php
+++ wp-show-more.php
@@ -3,7 +3,7 @@
 Plugin Name: WP show more
 Plugin URI:  http://plugins.wordpress.org/wp-show-more/
 Description: Add a user-defined link to display more content.
-Version:     1.0.3
+Version:     1.0.2
 Author:      JAMOS Web Service
 Author URI:  http://www.jamos.ch/plugins/wp-show-more
 License:     GPL2
@@ -11,22 +11,20 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: WP show more
 */
 
+/*
+ * Usage: [show_more title="Read more" color="#0066CC" list="-"] Your hidden Content [/show_more]
+ */
 
 add_shortcode( 'show_more', 'sm');
 
 function sm( $attr, $content ) {
   if (!isset($attr['color'])) $attr['color'] = '#cc0000';
   if (!isset($attr['list'])) $attr['list'] = '';
-  if (!isset($attr['more'])) $attr['more'] = 'show more';
-  if (!isset($attr['less'])) $attr['less'] = 'show less';
   $string  = '<div class="show_more">';
-  $string .= '<a class="wpsm-show" style="color: ' . $attr['color'] . ' ">'; 
-  $string .= $attr['list']. ' '  . $attr['more'];
-  $string .= '</a><div class="wpsm-content">';
+  $string .=' <p class="show_more_title" style="color: ' . $attr['color'] . ' ">'; 
+  $string .= $attr['list']. ' '  . $attr['title'];
+  $string .= '</p><div class="show_more_body" style="display:none">';
   $string .= $content;
-  $string .= '<a class="wpsm-hide" style="color: ' . $attr['color'] . ' ">'; 
-  $string .= $attr['list']. ' '  . $attr['less'];
-  $string .= '</a>';
   $string .= '</div></div>';
   return $string;
 }
@@ -47,4 +45,3 @@ function sm_scripts (){
   );
 }
 ?>
-
