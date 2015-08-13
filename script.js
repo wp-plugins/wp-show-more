c77dcc790211bbbfb7ad46b53be5a6d804e1394f
rene.maurer <rene.maurer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 08:25:29 +0000

    publish release 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-show-more/trunk@1219743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-jQuery( document ).ready( function(){
-   jQuery( '.show_more_title' ).
-   click( function(){
-	jQuery( this ).
-	parent().
-	children( '.show_more_body' ).
-	slideToggle();
-   });
+jQuery('.wpsm-content').addClass('wpsm-content-hide')
+jQuery('.wpsm-show, .wpsm-hide').removeClass('wpsm-content-hide')
+jQuery('.wpsm-show').on('click', function(e) {
+  jQuery(this).next('.wpsm-content').removeClass('wpsm-content-hide');
+  jQuery(this).addClass('wpsm-content-hide');
+  e.preventDefault();
+});
+jQuery('.wpsm-hide').on('click', function(e) {
+  var p = jQuery(this).parent('.wpsm-content');
+  p.addClass('wpsm-content-hide');
+  p.prev('.wpsm-show').removeClass('wpsm-content-hide');
+  e.preventDefault();
 });
